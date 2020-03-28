5951025ec1f60a9714a7f427073da56b33c60be9
ronwimmer <rafaapp2017@gmail.com>
2020-03-27 22:03:01 -0300

    Front-End do App

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,41 +14,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-/**
- * Rota / Recurso = é esse '/' do app.get... fica, no navegador, como localhost:3333/users
- */
-
-/**
- * Métodos HTTP:
- * 
- * GET: Buscar uma informação do back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deletar uma informação no back-end
- * 
- * Navegador só usa o método GET
- */
-
-/**
- * Tipos de parâmetros:
- * 
- * Query Params: Parâmetros nomeados enviados na rota após o símbolo "?" (Filtros, paginação, etc) (parâmetros separados entre si por "&")
- * Route Params: Parâmetros utilizados para identificar recursos na rota (rota, seguido de "/:id")
- * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
- */
-
-/**
- * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
- * NoSQL: MongoDB, CouchDB, etc
- */
-
-/**
- * Uso do banco de dados (através de):
- * 
- * Driver: SELECT * FROM users
- * Query Builder: table('users').select('*').where() - funciona com quase qualquer SQL, dependendo do Query Builder escolhido
- * 
- * Vamos usar o Query Builder 'Knex.js'
- */
-
 app.listen(3333);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,29 +7,6 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-/**
- * Rota / Recurso = é esse '/' do app.get... fica, no navegador, como localhost:3333/users
- */
-
-/**
- * Métodos HTTP:
- * 
- * GET: Buscar uma informação do back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deletar uma informação no back-end
- * 
- * Navegador só usa o método GET
- */
-
-/**
- * Tipos de parâmetros:
- * 
- * Query Params: Parâmetros nomeados enviados na rota após o símbolo "?" (Filtros, paginação, etc) (parâmetros separados entre si por "&")
- * Route Params: Parâmetros utilizados para identificar recursos na rota (rota, seguido de "/:id")
- * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
- */
-
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
